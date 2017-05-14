6ec36339139a4268f6374800c4ff78fe226d4c43
Apiwat Hantrakool <apiwat_hantrakool@cmu.ac.th>
2017-05-14 22:45:48 +0700

    แก้ CourseServiceImpTest

--- src/test/java/camt/cbsd/CourseServiceImpTest.java
+++ src/test/java/camt/cbsd/CourseServiceImpTest.java
@@ -1,21 +1,15 @@
 package camt.cbsd;
 import camt.cbsd.dao.CourseDao;
-import camt.cbsd.services.CourseService;
 import camt.cbsd.services.CourseServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
+
+
 /**
  * Created by User on 14/5/2560.
  */
